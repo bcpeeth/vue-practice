d109caebe28cac6133c53e3df91440f3e931da20
bcpeeth <34841898+bcpeeth@users.noreply.github.com>
2018-12-12 14:40:36 +0100

    finishes structure navbar

---
+++ navbar/src/components/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Contact Me</h1>
+  <p>
+    <ul>
+      <li>Email: {{email}}</li>
+      <li>Website: <a href="http://webolution.nl/">{{web}}</a></li>
+    </ul>
+  </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data(){
+    return {
+      email: 'pedro@pedro.com',
+      web: 'webolution.nl'
+    }
+  }
+}
+</script>
--- navbar/src/router/index.js
+++ navbar/src/router/index.js
@@ -1,14 +1,14 @@
-require('./main.scss');
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from './components/HelloWorld'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
+
+import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import About from '@/components/About'
+import Contact from '@/components/Contact'
 
 Vue.use(Router)
 
-export default new Router ({
+export default new Router({
   routes: [
     {
       path: '/',
@@ -20,7 +20,7 @@ export default new Router ({
       name: 'Home',
       component: Home
     },
-    {  
+    {
       path: '/about',
       name: 'About',
       component: About
@@ -31,11 +31,4 @@ export default new Router ({
       component: Contact
     }
   ]
-})
-
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
 })
