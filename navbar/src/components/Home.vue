64cf02bb1e865fa8a372c69a6211ebfda9ac3836
bcpeeth <34841898+bcpeeth@users.noreply.github.com>
2018-12-20 10:54:33 +0100

    merge conflict?

--- navbar/src/components/Home.vue
+++ navbar/src/components/Home.vue
@@ -7,13 +7,13 @@
       <h1>Home</h1>
       <p>Here is some information. This is the home page</p>
 
-      <div class="row text-block">    
+      <div class="row text-block">
         <div class="col">
           <h2>One Title</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div class="col">
-          <h2>Other Title</h2>   
+          <h2>Other Title</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
         </div>
       </div>
